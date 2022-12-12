51f0d33b8d6de88ce8ffdd08fc5f22a91d252c9f
ertfm <ertfm@users.noreply.github.com>
2022-12-12 20:30:15 -0300

    schedule ping for already created hosts

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,24 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from apscheduler.schedulers.background import BackgroundScheduler
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 
 db = SQLAlchemy(app)
+scheduler = BackgroundScheduler()
+
+scheduler.start()
 
 from app.models import Host
+from app.services import get_hosts, schedule_ping
 
 with app.app_context():
     db.create_all()
+    hosts = get_hosts()
+    if hosts:
+        schedule_ping(hosts)
+
+
+
+    
---
+++ app/services.py
@@ -0,0 +1,15 @@
+from app import db, scheduler
+from app.models import Host
+from subprocess import call, DEVNULL
+
+def get_hosts():
+    hosts = [ host for host in db.session.execute(db.select(Host)).scalars()]
+    return hosts
+
+def schedule_ping(hosts):
+    for host in hosts:
+        scheduler.add_job(ping,'interval',seconds=30, id=str(host.id), args=[host])
+
+def ping(host):
+    exit_code = call(['ping','-c','1',host.hostname], stdout=DEVNULL, stderr=DEVNULL)
+    print(f'{host.hostname}:{exit_code}')
