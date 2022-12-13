88b7ec441e527e0d5784e3849e7d5a0c55dc9e0f
ertfm <119823585+ertfm@users.noreply.github.com>
2022-12-13 12:31:34 -0300

    on successful host creation, add host box

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,4 +1,4 @@
-
+dashboardDiv = document.querySelector('#dashboard')
 addHostBtn = document.querySelector('#add-host-btn');
 addHostModal = document.querySelector('#add-host-modal');
 addHostModalCloseBtn = document.querySelector('#add-host-modal-close-btn')
@@ -8,6 +8,28 @@ hostnameText = addHostForm.querySelector('#hostname')
 
 
 const socket = io();
+
+function addHostBox(host) {
+    console.log(host)
+    boxDiv = document.createElement('div')
+    boxDiv.id = host.friendly_name
+    boxDiv.className = 'box'
+
+    friendlyNameHeader = document.createElement('h5')
+    friendlyNameHeader.textContent = host.friendly_name
+
+    hostnameSmall = document.createElement('small')
+    hostnameSmall.textContent = host.hostname
+
+    statusSpan = document.createElement('span')
+    statusSpan.className = 'status'
+    statusSpan.textContent = '-'
+
+    boxDiv.appendChild(friendlyNameHeader);
+    boxDiv.appendChild(hostnameSmall);
+    boxDiv.appendChild(statusSpan)
+    dashboardDiv.appendChild(boxDiv);
+}
 
 addHostBtn.addEventListener('click', () => {
     addHostModal.setAttribute('open', true);
@@ -27,4 +49,14 @@ addHostForm.addEventListener('submit', (e) => {
         'friendly-name':friendlyName,
         'hostname':hostname
     })
+})
+
+
+socket.on('server:add-host', (response) => {
+    console.log(response)
+    if (response.status == 'success') {
+        addHostBox(response.data)
+        addHostModal.removeAttribute('open')
+        addHostForm.reset()
+    }
 })
