7101db2673a3e2e80d80c58e7c63c59056647aab
ertfm <119823585+ertfm@users.noreply.github.com>
2022-12-14 12:39:39 -0300

    small fixes

--- app/socket_handlers.py
+++ app/socket_handlers.py
@@ -5,19 +5,17 @@ from app.services import add_host, get_hosts
 def connection_handler():
     try:
         hosts = [ host.to_json() for host in get_hosts() ]
-        #print(hosts)
         if hosts:
             socket.emit('server:send-hosts', { 'status':'success', 'data':hosts})
 
     except Exception as e:
-        return { 'status':'error', 'data': 'here goes a descriptive error'}
+        socket.emit('server:send-hosts', { 'status':'error', 'data': 'here goes a descriptive error'})
 
 @socket.on('client:add-host')
-def add_host_handler(request):
+def add_host_handler(new_host):
     try:
-        host = add_host(request)
-
+        host = add_host(new_host)
         socket.emit('server:add-host', {'status': 'success','data': host.to_json(), 'message': 'Host added successfuly'})
 
     except Exception as e:
-        return { 'status': 'error', 'data': '', 'message': 'Here goes a descriptive error'}
+        socket.emit('server:add-host',{ 'status': 'error', 'data': '', 'message': 'Here goes a descriptive error'})
--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,33 +1,32 @@
-dashboardDiv = document.querySelector('#dashboard')
-addHostBtn = document.querySelector('#add-host-btn');
-addHostModal = document.querySelector('#add-host-modal');
-addHostModalCloseBtn = document.querySelector('#add-host-modal-close-btn')
-addHostForm = document.querySelector('#add-host-form')
-friendlyNameText = addHostForm.querySelector('#friendly-name')
-hostnameText = addHostForm.querySelector('#hostname')
-
+const dashboardDiv = document.querySelector('#dashboard');
+const addHostBtn = document.querySelector('#add-host-btn');
+const addHostModal = document.querySelector('#add-host-modal');
+const addHostModalCloseBtn = document.querySelector('#add-host-modal-close-btn');
+const addHostForm = document.querySelector('#add-host-form');
+const friendlyNameText = addHostForm.querySelector('#friendly-name');
+const hostnameText = addHostForm.querySelector('#hostname');
 
 const socket = io();
 
 function addHostBox(host) {
-    console.log(host)
-    boxDiv = document.createElement('div')
-    boxDiv.id = host.friendly_name
-    boxDiv.className = 'box'
+    console.log(host);
+    const boxDiv = document.createElement('div');
+    boxDiv.id = host.friendly_name;
+    boxDiv.className = 'box';
 
-    friendlyNameHeader = document.createElement('h5')
-    friendlyNameHeader.textContent = host.friendly_name
+    const friendlyNameHeader = document.createElement('h5');
+    friendlyNameHeader.textContent = host.friendly_name;
 
-    hostnameSmall = document.createElement('small')
-    hostnameSmall.textContent = host.hostname
+    const hostnameSmall = document.createElement('small');
+    hostnameSmall.textContent = host.hostname;
 
-    statusSpan = document.createElement('span')
-    statusSpan.className = 'status'
-    statusSpan.textContent = '-'
+    const statusSpan = document.createElement('span');
+    statusSpan.className = 'status';
+    statusSpan.textContent = '-';
 
     boxDiv.appendChild(friendlyNameHeader);
     boxDiv.appendChild(hostnameSmall);
-    boxDiv.appendChild(statusSpan)
+    boxDiv.appendChild(statusSpan);
     dashboardDiv.appendChild(boxDiv);
 }
 
@@ -37,34 +36,34 @@ addHostBtn.addEventListener('click', () => {
 
 addHostModalCloseBtn.addEventListener('click', () => {
     addHostModal.removeAttribute('open');
-})
+});
 
 addHostForm.addEventListener('submit', (e) => {
     e.preventDefault();
 
-    friendlyName = friendlyNameText.value
-    hostname = hostnameText.value
+    friendlyName = friendlyNameText.value;
+    hostname = hostnameText.value;
 
     socket.emit('client:add-host', {
         'friendly-name':friendlyName,
         'hostname':hostname
-    })
-})
+    });
+});
 
 socket.on('server:send-hosts', (response) => {
     if (response.status == 'success') {
         response.data.forEach(host => {
-            addHostBox(host)
+            addHostBox(host);
         });
     }
  
-})
+});
 
 socket.on('server:add-host', (response) => {
-    console.log(response)
+    console.log(response);
     if (response.status == 'success') {
-        addHostBox(response.data)
-        addHostModal.removeAttribute('open')
-        addHostForm.reset()
+        addHostBox(response.data);
+        addHostModal.removeAttribute('open');
+        addHostForm.reset();
     }
-})
+});
